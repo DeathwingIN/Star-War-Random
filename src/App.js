1f38211903e0f4f78e4654fe5e5c2f3c54926e79
DeathwingIN <imeshnirmal1u@gmail.com>
2023-03-12 22:03:05 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -3,37 +3,60 @@ import "./App.css";
 // import Item from './MyItem';
 import React from "react";
 
-class StarWars extends React.Component {
+// class FilmItemRow extends React.Component {
+//   render(){
+//     return(
+//         <li>
+//           <a href={this.props.url}>{this.props.url}</a>
+//         </li>
+//     )
+//   }
+// }
 
-  constructor(){
-    super()
+class StarWars extends React.Component {
+  constructor() {
+    super();
     this.state = {
-      name:null,
-      height:null,
-      homeworld:null,
-      films:[],
-    }
+      loadedCharacter: false,
+      name: null,
+      height: null,
+      image: null,
+      homeworld: null,
+    };
   }
 
   getNewCharactor() {
-    console.log("Ok");
-    this.setState({
-      name:"person1",
-      height:"150",
-      homeworld:'null',
-      films:['item 1', 'item 2'],
-    })
+    const randomNumber = Math.round(Math.random() * 88);
+    const url = `https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id/${randomNumber}.json`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          name: data.name,
+          height: data.height,
+          homeworld: data.homeworld,
+          image: data.image,
+          loadedCharacter: true,
+        });
+      });
   }
 
   render() {
+    // Capitalize first letter of homeworld
+
     return (
       <div>
-        <h1>{this.state.name}</h1>
-        <p> {this.state.height} cm</p>
-        <p>HomeWorld: {this.state.homeworld}</p>
-        <ul>
-          <li>{this.state.films}</li>
-        </ul>
+        {this.state.loadedCharacter && (
+          <div
+            // className="bg-img"
+            // style={{ backgroundImage: `url(${this.stat.image})`}}
+          >
+            <img className="imageCSS" src={this.state.image}></img>
+            <h1>{this.state.name}</h1>
+            <p> {this.state.height} m</p>
+            <p>HomeWorld:&nbsp;{this.state.homeworld}</p>
+          </div>
+        )}
         <button
           type="butoon"
           className="btn"
