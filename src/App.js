5158732974a29965cd40e11f823e3962f164f4cc
DeathwingIN <imeshnirmal1u@gmail.com>
2023-03-12 08:17:20 +0530

    Last few

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // import Item from './MyItem';
-import React from 'react';
+import React from "react";
 
-class StarWars extends React.Component{
-render(){
-  return(
-    <h1>This is Star Wars </h1>
-  )
+class StarWars extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      name:null,
+      height:null,
+      homeworld:null,
+      films:[],
+    }
+  }
+
+  getNewCharactor() {
+    console.log("Ok");
+    this.setState({
+      name:"person1",
+      height:"150",
+      homeworld:'null',
+      films:['item 1', 'item 2'],
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <p> {this.state.height} cm</p>
+        <p>HomeWorld: {this.state.homeworld}</p>
+        <ul>
+          <li>{this.state.films}</li>
+        </ul>
+        <button
+          type="butoon"
+          className="btn"
+          onClick={() => this.getNewCharactor()}
+        >
+          Randomize Charactor
+        </button>
+      </div>
+    );
+  }
 }
-
-}
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <StarWars></StarWars>
+        <StarWars></StarWars>
       </header>
     </div>
   );
