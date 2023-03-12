a3bc9d002a2e878bd240ed62d826f7e7c76e03d6
DeathwingIN <imeshnirmal1u@gmail.com>
2023-03-12 08:16:33 +0530

    last few

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+// import Item from './MyItem';
+import React from 'react';
+
+class StarWars extends React.Component{
+render(){
+  return(
+    <h1>This is Star Wars </h1>
+  )
+}
+
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <StarWars></StarWars>
       </header>
     </div>
   );
